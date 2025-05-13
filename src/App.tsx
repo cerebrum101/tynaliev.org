e19835e49aa0d9e9d59881e64c99a00e610508a7
cerebrum101 <myusername22.05@gmail.com>
2025-05-13 13:41:56 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="font-bold text-center text-4xl">Тыналиев Астар</h1>
+        <p className="text-center">Джамиля</p>
+      </div>
+    </>
+  )
+}
+
+export default App
